13b705d9e61d7d88624480cd23079d5f3d217290
Brandon Lyon <etherous@gmail.com>
2020-03-20 16:58:59 -0600

    Changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,12 +4,12 @@ cd /chappie
 cd chappie-common
 git checkout master
 git pull
-mvn clean install
+mvn clean install -DskipTests
 cd ../
 cd chappie-server
 git checkout master
 git pull
-mvn clean install
+mvn clean install -DskipTests
 
 java -jar $(ls /chappie/chappie-server/target/chappie-server*.jar | head -n 1)
 
