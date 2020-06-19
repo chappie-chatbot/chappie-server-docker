1e11a3ec33f35682382e5637a1c75182beb796fd
Brandon Lyon <etherous@gmail.com>
2020-06-19 17:01:47 -0600

    Chappie 2.0 changes

--- Dockerfile
+++ Dockerfile
@@ -13,6 +13,7 @@ RUN mkdir -p $HOMEDIR && chown -R chappie $HOMEDIR
 USER chappie
 
 RUN git clone https://github.com/chappie-chatbot/chappie-common.git chappie-common \
+    && git clone https://github.com/chappie-chatbot/chappie-webui.git chappie-webui \
     && git clone https://github.com/chappie-chatbot/chappie-server.git chappie-server
 
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -6,9 +6,16 @@ git checkout master
 git pull
 mvn clean install -DskipTests
 cd ../
+cd chappie-webui
+git checkout master
+git pull
+cd ../
 cd chappie-server
 git checkout master
 git pull
+pwd
+mkdir -p src/main/resources/static/
+cp ../chappie-webui/src/* src/main/resources/static/
 mvn clean install -DskipTests
 
 java -jar $(ls /chappie/chappie-server/target/chappie-server*.jar | head -n 1)
